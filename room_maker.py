072afd59bf684b9054e13a2998a523f4650cb63b
Isaiah Harville <harvillerisaiah@gmail.com>
2020-10-10 02:48:45 -0500

    Changed spacing on room maker

--- room_maker.py
+++ room_maker.py
@@ -1,5 +1,4 @@
-# Program for automating the development of rooms.\
-
+# Program for automating the development of rooms.
 import pygame
 pygame.init()
 
@@ -30,7 +29,6 @@ class RoomDisplay:
 
     def DrawLine(self, room2):
         pygame.draw.line(screen, (255, 255, 255), (self.rect.x + 10, self.rect.y + 20), (room2.rect.x + 20, room2.rect.y + 20))
-
 
 
 rooms = [RoomDisplay((750, 400), 0)]
@@ -83,6 +81,7 @@ while not done:
                                 rooms[selectedRoomNum].room.up = roomNum  
                         break
 
+
                 if not foundRoom:
                     if xDistance > yDistance:
                         rooms.append(RoomDisplay((pos[0] - 10, rooms[selectedRoomNum].rect.y), len(rooms)))
@@ -100,7 +99,6 @@ while not done:
                         else: # If room is being added up
                             rooms[selectedRoomNum].room.up = len(rooms) - 1
                             rooms[-1].room.down = selectedRoomNum                   
-
 
 
             else:
