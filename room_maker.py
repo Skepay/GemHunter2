fc89db6b64295bb00a33f929b3281de260e2d19e
Josh Payne <jaypay0412@gmail.com>
2020-10-10 02:40:18 -0500

    More work down to room maker

--- room_maker.py
+++ room_maker.py
@@ -18,20 +18,99 @@ class Room: # stores attributes of each room
     NPC = None # str
 
 class RoomDisplay:
-    def __init__(self, coords):
+    def __init__(self, coords, roomNum):
         self.room = Room()
-        self.coords = coords
+        self.room.name = roomNum
+        self.rect = pygame.Rect(coords[0], coords[1], 20, 20)
 
     def drawRoom(self):
-        pygame.draw.rect(screen, (255, 255, 255), (self.coords[0], self.coords[1], 20, 20))
+        pygame.draw.rect(screen, (255, 255, 255), self.rect)
 
         text = font.render('%g'%self.room.name, True, (0, 0, 0))
-        screen.blit(text, (self.coords[0], self.coords[1]))
+        screen.blit(text, (self.rect.x + 1, self.rect.y + 1))
 
     def DrawLine(self, room2):
-        pygame.draw.line(screen, (255, 255, 255), (self.coords[0] + 10, self.coords[1] + 20), (room2.coords[0] + 20, room2.coords[1] + 20))
+        pygame.draw.line(screen, (255, 255, 255), (self.rect.x + 10, self.rect.y + 20), (room2.rect.x + 20, room2.rect.y + 20))
 
 
 
-rooms = [Room()]
-Rooms[0] = 0
+rooms = [RoomDisplay((750, 400), 0)]
+
+done = False
+makingRoom = False
+
+while not done:
+    screen.fill((0, 0, 0))
+
+    for room in rooms:
+        room.drawRoom()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos = pygame.mouse.get_pos()
+
+            if makingRoom:
+                makingRoom = False
+
+                # Calculating the distance from mouse cursor to room in both axes
+                xDistance = abs(pos[0] - rooms[selectedRoomNum].rect.x)
+                yDistance = abs(pos[1] - rooms[selectedRoomNum].rect.y)
+
+                foundRoom = False
+                for roomNum in range(len(rooms)):
+                    if rooms[roomNum].rect.collidepoint(pos):
+                        if roomNum == selectedRoomNum:
+                            break
+                        
+                        foundRoom = True
+
+                        if xDistance > yDistance: # If room is being added on the X axis
+                            if rooms[roomNum].rect.x > rooms[selectedRoomNum].rect.x: # If room pressed is to the right
+                                rooms[roomNum].room.left = selectedRoomNum
+                                rooms[selectedRoomNum].room.right = roomNum       
+                            else: # If room pressed is to the left
+                                rooms[roomNum].room.right = selectedRoomNum
+                                rooms[selectedRoomNum].room.left = roomNum  
+                        else: # If room is being added on the Y axis
+                            if rooms[roomNum].rect.y > rooms[selectedRoomNum].rect.y: # If room pressed is down
+                                rooms[roomNum].room.up = selectedRoomNum
+                                rooms[selectedRoomNum].room.down = roomNum       
+                            else: # If room pressed is up
+                                rooms[roomNum].room.down = selectedRoomNum
+                                rooms[selectedRoomNum].room.up = roomNum  
+                        break
+
+                if not foundRoom:
+                    if xDistance > yDistance:
+                        rooms.append(RoomDisplay((pos[0] - 10, rooms[selectedRoomNum].rect.y), len(rooms) - 1))
+                        if rooms[-1].rect.x > rooms[selectedRoomNum].rect.x: # If room is being added to the right
+                            rooms[selectedRoomNum].room.right = len(rooms) - 1
+                            rooms[-1].room.left = selectedRoomNum
+                        else: # If room is being added to the left
+                            rooms[selectedRoomNum].room.left = len(rooms) - 1
+                            rooms[-1].room.right = selectedRoomNum
+                    else:
+                        rooms.append(RoomDisplay((rooms[selectedRoomNum].rect.x, pos[1] - 10), len(rooms) - 1))
+                        if rooms[-1].rect.y > rooms[selectedRoomNum].rect.y: # If room is being added down
+                            rooms[selectedRoomNum].room.down = len(rooms) - 1
+                            rooms[-1].room.up = selectedRoomNum
+                        else: # If room is being added up
+                            rooms[selectedRoomNum].room.up = len(rooms) - 1
+                            rooms[-1].room.down = selectedRoomNum                   
+
+
+
+            else:
+                for roomNum in range(len(rooms)):
+                    if rooms[roomNum].rect.collidepoint(pos):
+                        selectedRoomNum = roomNum
+                        makingRoom = True
+                        break
+
+
+
+    pygame.display.update()
