5e9eb43ab2333ef32775e1afede26b60b65a3a29
Isaiah Harville <harvillerisaiah@gmail.com>
2020-10-12 16:50:25 -0500

    im not sure what changes i have here but im going to push anyways

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from npc import *
 
 # Intro to Game
 # TODO: Uncomment this for production -> Introduction()
-Introduction()
 
 #~/ Main Loop \~#
 while 1:
     InfoMessages()
 
     # Travel options
-    roomIndex = rooms.index(Player.room)    # thisstupidly long and ugly print statement below, will type out move locations in blue
+    roomIndex = rooms.index(Player.room)    # this stupidly long and ugly print statement below, will type out move locations in blue
     print("up:",color("Room %s,"%rooms[roomIndex].up, TextColor.blue),"down:",color("Room %s,"%rooms[roomIndex].down, TextColor.blue),"left:",color("Room %s,"%rooms[roomIndex].left, TextColor.blue),"right:",color("Room %s,"%rooms[roomIndex].right, TextColor.blue))
 
     # Players next location
